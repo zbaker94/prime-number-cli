301dff993910fca1db22daca44e59425b493057f
zbaker94 <zbaker94@gmail.com>
2024-05-30 17:15:47 -0400

    fixed the rest of the tests

--- tests/test_prime_number.py
+++ tests/test_prime_number.py
@@ -9,7 +9,8 @@ runner = CliRunner()
 # test the prime number generator (sieve of eratosthenes) for all
 # numbers between 2 and 100
 def test_sieve_of_eratosthenes():
-    prime_cantidates = prime.sieve_of_eratosthenes(100)
+    (prime_cantidates, sieve_status) = prime.sieve_of_eratosthenes(100)
+    assert sieve_status == SUCCESS
     prime_count = filter(lambda x: x is True, prime_cantidates)
     # check that we have 25 prime numbers between 2 and 100
     assert len(list(prime_count)) == 25
@@ -18,80 +19,110 @@ def test_sieve_of_eratosthenes():
     assert prime_cantidates[1] is False
     assert prime_cantidates[2] is True
     # check that we got the expected prime numbers
-    assert prime.bool_array_to_prime_array(prime_cantidates) == [
+    (prime_numbers, convert_status) = prime.bool_array_to_prime_array(prime_cantidates)
+    assert prime_numbers == [
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
         67, 71, 73, 79, 83, 89, 97
     ]
+    assert convert_status == SUCCESS
 
 
 # test if 7 is prime
 def test_is_prime_7():
-    assert prime.is_prime(7) is True
+    (is_prime, status_code) = prime.is_prime(7)
+    assert is_prime is True
+    assert status_code == SUCCESS
 
 
 # test if 9 is not prime
 def test_is_prime_9():
-    assert prime.is_prime(9) is False
+    (is_prime, status_code) = prime.is_prime(9)
+    assert is_prime is False
+    assert status_code == SUCCESS
 
 
 # test if 1 is prime
 def test_is_prime_1():
-    assert prime.is_prime(1) is False
+    (is_prime, status_code) = prime.is_prime(1)
+    assert is_prime is False
+    assert status_code == SUCCESS
 
 
 # test if 0 is prime
 def test_is_prime_0():
-    assert prime.is_prime(0) is False
+    (is_prime, status_code) = prime.is_prime(0)
+    assert is_prime is False
+    assert status_code == SUCCESS
 
 
 # test a list of numbers to see if they are prime (all prime)
 def test_is_prime_list_all_prime():
-    assert prime.is_prime_list([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]) == [
+    (prime_list, status_code) = prime.is_prime_list(
+        [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
+    assert prime_list == [
         {2: True}, {3: True}, {5: True}, {7: True}, {11: True}, {13: True},
         {17: True}, {19: True}, {23: True}, {29: True}
     ]
+    assert status_code == SUCCESS
 
 
 # test a list of numbers to see if they are prime (all not prime)
 def test_is_prime_list_all_not_prime():
-    assert prime.is_prime_list([4, 6, 8, 9, 10, 12, 14, 15, 18, 21]) == [
+    (prime_list, status_code) = prime.is_prime_list(
+        [4, 6, 8, 9, 10, 12, 14, 15, 18, 21])
+    assert prime_list == [
         {4: False}, {6: False}, {8: False}, {9: False}, {10: False},
         {12: False}, {14: False}, {15: False}, {18: False}, {21: False}
     ]
+    assert status_code == SUCCESS
 
 
 # test a list of numbers to see if they are prime (mixed)
 def test_is_prime_list_mixed():
-    assert prime.is_prime_list([2, 4, 5, 6, 7, 9, 11, 12, 13, 15]) == [
+    (prime_list, status_code) = prime.is_prime_list(
+        [2, 4, 5, 6, 7, 9, 11, 12, 13, 15])
+    assert prime_list == [
         {2: True}, {4: False}, {5: True}, {6: False}, {7: True},
         {9: False}, {11: True}, {12: False}, {13: True}, {15: False}
     ]
+    assert status_code == SUCCESS
 
 
 # test a list of numbers to see if they are prime (empty)
 def test_is_prime_list_empty():
-    assert prime.is_prime_list([]) == []
+    (prime_list, status_code) = prime.is_prime_list([])
+    assert prime_list == []
+    assert status_code == SUCCESS
 
 
 # test an arbitrary range [7-82] for prime numbers
 def test_prime_numbers_between():
-    assert prime.prime_numbers_between(lower_bound=7, upper_bound=82) == [
+    (prime_range, status_code) = prime.prime_numbers_between(
+        lower_bound=7, upper_bound=82)
+    assert prime_range == [
         7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
         73, 79
     ]
+    assert status_code == SUCCESS
 
 
 # test an arbitrary inverted range [82-7] for prime numbers
 def test_prime_numbers_between_inverted():
-    assert prime.prime_numbers_between(lower_bound=82, upper_bound=7) == [
+    (prime_range, status_code) = prime.prime_numbers_between(
+        lower_bound=82, upper_bound=7)
+    assert prime_range == [
         7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
         73, 79
     ]
+    assert status_code == SUCCESS
 
 
 # test range where both bounds are the same [7-7] prime number
 def test_prime_numbers_between_same_prime():
-    assert prime.prime_numbers_between(lower_bound=7, upper_bound=7) == [7]
+    (prime_range, status_code) = prime.prime_numbers_between(
+        lower_bound=7, upper_bound=7)
+    assert prime_range == [7]
+    assert status_code == SUCCESS
 
 
 # test range where both bounds are the same [4-4] non-prime number
